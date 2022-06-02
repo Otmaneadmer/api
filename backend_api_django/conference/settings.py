5cf7abde905365cb395b920ba748262dd7126671
admer otmane <othmanad155@gmail.com>
2022-06-02 14:03:06 +0100

    update in heroku

--- backend_api_django/conference/settings.py
+++ backend_api_django/conference/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+import dj_database_url
+import django_heroku
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(n)+4wgur+2-avd968=6hy=mp2fi!lx)a+a_1yo&3m*1ao3y4%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS=[]
+#remove https://from the url
+#use * if you want to allow all hosts.
+ALLOWED_HOSTS = ['<url>']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -57,7 +62,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',   
+    'django.middleware.security.SecurityMiddleware', 
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
 
 ]
 
@@ -96,6 +103,9 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+DATABASES['default'] = dj_database_url.config(
+    default='mysql://root:<password>@localhost:3306/<database>',
+)
 
 
 # Password validation
@@ -132,11 +142,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+django_heroku.settings(locals())
